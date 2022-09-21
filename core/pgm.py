4cbd39be73ae0b807f1b9a8b1055c7aca8e2cd63
Aadam <aadimator@gmail.com>
2022-09-21 12:51:58 +0500

    feat: Add Schema, Get States, Reformat Config

--- core/pgm.py
+++ core/pgm.py
@@ -1,3 +1,5 @@
+from enum import Enum
+from types import SimpleNamespace
 from pgmpy.models import BayesianNetwork
 from omegaconf import DictConfig, OmegaConf
 from pgmpy.factors.discrete import TabularCPD
@@ -9,23 +11,34 @@ class PGM:
   def __init__(self, cfg: DictConfig):
     self.cfg = cfg
     self.model = BayesianNetwork(cfg.model.edges)
-    self.states =  OmegaConf.to_container(cfg.model.get("states", None))
+    self.states =  cfg.model.get("states", None)
+    if self.states:
+      self.states = OmegaConf.to_container(self.states)
+    
     cpds = []
-    for cpd in cfg.model.cpd:
+    for variable in cfg.model.cpd:
       state_names = {}
+      cpd = cfg.model.cpd[variable]
       evidence = cpd.get('evidence', None)
-      evidence_card = cpd.get('evidence_card', None)
+      evidence_card = None
+      if evidence:
+        evidence_card = [cfg.model.cpd[e].cardinality for e in evidence]
       if self.states:
-        state_names[cpd.variable] = self.states[cpd.variable]
+        state_names[variable] = self.states[variable]
         if evidence:
           state_names.update({e:self.states[e] for e in cpd.evidence})
       
       cpds.append(
-        TabularCPD(variable=cpd.variable, variable_card=cpd.cardinality, 
+        TabularCPD(variable=variable, variable_card=cpd.cardinality, 
           values=cpd.probabilities, evidence=evidence, evidence_card=evidence_card, state_names=state_names))
+
     self.model.add_cpds(*cpds)
     assert self.model.check_model() == True
     self.infer = VariableElimination(self.model)
+
+  def get_states(self):
+    states = {self.cfg.model.cpd[k].name: Enum(f'{k}', self.states[k]) for k, v in self.states.items()}
+    return SimpleNamespace(**states)
 
   def print_cpd(self, variable: str):
     print(self.model.get_cpds(variable))
---
+++ shcemas/pgm_schema.py
@@ -0,0 +1,21 @@
+from dataclasses import dataclass, field
+from typing import Any, Dict, List, Optional
+from omegaconf import MISSING
+
+@dataclass
+class PGMCPD:
+    name: str = MISSING
+    cardinality: int = MISSING
+    probabilities: List[List[float]] = field(default_factory=list)
+    evidence: Optional[List[str]] = None
+    evidence_card: Optional[List[int]] = None
+
+@dataclass
+class PGMModel:
+    edges: List[List[str]] = field(default_factory=list)
+    states: Optional[Dict[str, List[str]]] = None
+    cpd: Dict[str, PGMCPD] = field(default_factory=dict)
+
+@dataclass
+class PGMSchema:
+    model: Any = MISSING
